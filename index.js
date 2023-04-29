b87c201de12930e4407979ee794c8af3b1a7369c
Mark Poepping <mark.poepping@admiralcloud.com>
2023-04-29 22:32:18 +0200

    [master] feat(App): Use modern OpenSearch connector | MP
    Use the modern OpenSearch connector as well as the modern AWS signer and use async/await
    Related issues:
    - undefined/browse/master
    
    BREAKING: Version uses async/await

--- index.js
+++ index.js
@@ -1,210 +1,169 @@
-const async = require('async')
 const _ = require('lodash') 
-const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid')
 
-const { Client, Connection } = require('@opensearch-project/opensearch')
-const AWS = require('aws-sdk')
+const { Client } = require('@opensearch-project/opensearch')
+const { fromNodeProviderChain } = require('@aws-sdk/credential-providers')
+
+const createAwsOpensearchConnector = require('aws-opensearch-connector')
 
 /**
- * 
- * @param options.providerConfigType STRING OPTIONAL - set type of aws accessKeys, defaults to secrets
+ * https://docs.aws.amazon.com/opensearch-service/latest/developerguide/request-signing.html#request-signing-node
+ * https://www.npmjs.com/package/aws-opensearch-connector
+ * https://opensearch-project.github.io/opensearch-js/2.1/index.html
  * @param options.createMapping FUNCTION OPTION - function to call if you want to rebuild ES indices during test
  **/
 
 module.exports = (acapi) => {
 
-  const init = function(options, cb) {
-    const providerConfig = _.find(_.get(acapi.config, 'aws.accessKeys'), { default: true, type: _.get(options, 'providerConfigType', 'secrets') })
+  const getClient = async ({ instance, server, index, region = 'eu-central-1', profile = process.env['profile'] }) => {
+    const protocol = _.get(acapi.config, 'localElasticSearch.protocol') || _.get(server, 'protocol', 'https')
+    const host = _.get(acapi.config, 'localElasticSearch.host') ||  _.get(server, 'host', 9200)
+    const port =  _.get(acapi.config, 'localElasticSearch.port') ||  _.get(server, 'port')
+    const url =  `${protocol}://${host}:${port}`
 
+    const credentials = await fromNodeProviderChain({ profile, ignoreCache: true })()
+    const esConfig = {
+      node: {
+        url: new URL(url),
+        ssl: {
+          // allow different certificate for SSH tunnel on NON-production system
+          rejectUnauthorized: acapi.config.environment === 'production'
+        }  
+      },
+      auth: _.get(server, 'auth'),
+      requestTimeout: acapi.config.elasticSearch.timeout,
+    }
+
+    if (!acapi.config.localElasticSearch && _.get(server, 'awsCluster')) {
+      const osConnector = createAwsOpensearchConnector({
+        credentials, 
+        region
+      })
+      _.merge(esConfig, osConnector)
+    }
+    acapi.elasticSearch[instance] = new Client(esConfig)
+    
+    const serverInfo = {
+      instance,
+      index: _.get(index, 'index'),
+      host,
+      port
+    }
+    // check version
+    try {
+      const result = await acapi.elasticSearch[instance].cluster.stats()
+      serverInfo.cluster = _.get(result, 'body.cluster_name')
+      serverInfo.clusterVersion = _.get(result, 'body.nodes.versions[0]')
+
+      // check if index exists
+      await acapi.elasticSearch[instance].indices.exists({ index: _.get(index, 'index') })
+    }
+    catch(e) {
+      console.log(84, e)
+    }
+
+    acapi.aclog.serverInfo(serverInfo)
+  }
+
+  const init = async() => {
     acapi.aclog.headline({ headline: 'ELASTICSEARCH' })
     
     // init multiple instances for different purposes
     acapi.elasticSearch = {}
+    
+    let indices = acapi.config.elasticSearch.indices || []
+    // filter out indices with omitInTest = true
+    if (acapi.config.environment === 'test') {
+      indices = _.filter(indices, index => {
+        if (!index.omitInTest) return index
+      })
+    }
   
-        // init multiple instances for different purposes
-    async.eachSeries(acapi.config.elasticSearch.indices, (index, itDone) => {
-      if (acapi.config.environment === 'test' && _.get(index, 'omitInTest')) return itDone()
+    for (const index of indices) {
+      if (acapi.config.environment === 'test' && _.get(index, 'omitInTest')) return 
       let instance = _.get(index, 'instance')
       let server = _.find(acapi.config.elasticSearch.servers, { server: _.get(index, 'server') })
-      if (!server) return itDone('serverConfigurationMissingForES')
+      if (!server) throw new Error({ message: 'serverConfigurationMissingForES' })
 
       // update config with environment if not global
-      let pos = _.findIndex(acapi.config.elasticSearch.indices, { model: index.model })
+      let pos = _.findIndex(indices, { model: index.model })
       index.index = (!index.global ? acapi.config.environment + '_' : '') + (process.env.NODE_TEST_ORIGIN ? process.env.NODE_TEST_ORIGIN + '_' : '' ) + (index.indexInfix || index.model)
-      acapi.config.elasticSearch.indices.splice(pos, 1, index)
+      indices.splice(pos, 1, index)
 
-      // check if instance is already created
+      // check if instance is already created - do not instanciate multiple times, we can re-use it
       if (_.has(acapi.elasticSearch, instance)) {
         acapi.aclog.serverInfo({
           instance,
           index: _.get(index, 'index')
         })
-        return itDone()
       }
-
-      // instanciate ES for this database/index
-      const protocol = _.get(acapi.config, 'localElasticSearch.protocol') || _.get(server, 'protocol', 'https')
-      const host = _.get(acapi.config, 'localElasticSearch.host') ||  _.get(server, 'host', 9200)
-      const port =  _.get(acapi.config, 'localElasticSearch.port') ||  _.get(server, 'port')
-
-      let url =  `${protocol}://${host}:${port}`
-      let esConfig = {
-        node: {
-          url: new URL(url),
-          ssl: {
-            // allow different certificate for SSH tunnel on NON-production system
-            rejectUnauthorized: acapi.config.environment === 'production'
-          }  
-        },
-        auth: _.get(server, 'auth'),
-        requestTimeout: acapi.config.elasticSearch.timeout,
-        providerConfig
+      else {
+        // create an elasticsearch client for your Amazon ES
+        await getClient({ instance, server, index })
       }
-
-      class AwsConnector extends Connection {
-        async request(params, callback) {
-          try {
-            const creds = providerConfig
-            const req = this.createRequest(params);
-      
-            const { request: signedRequest } = this.signRequest(req, creds);
-            super.request(signedRequest, callback);
-          }
-          catch (error) {
-            acapi.log.error('AWSESConnector | Request failed %j', error)
-            throw error;
-          }
-        }
-      
-        createRequest(params) {
-          const endpoint = new AWS.Endpoint(this.url.href);
-          let req = new AWS.HttpRequest(endpoint);
-
-          Object.assign(req, params);
-          if (req.querystring) {
-            req.path += `/?${req.querystring}`;
-            delete req.querystring;  
-          }
-
-          req.region = _.get(providerConfig, 'region', 'eu-central-1')
-      
-          if (!req.headers) {
-            req.headers = {};
-          }
-      
-          let body = params.body;
-      
-          if (body) {
-            let contentLength = Buffer.isBuffer(body)
-              ? body.length
-              : Buffer.byteLength(body);
-            req.headers['Content-Length'] = contentLength;
-            req.body = body;
-          }
-          req.headers['Host'] = (acapi.config.environment !== 'production' ? 'localhost' : endpoint.host)        
-          return req;
-        }
-      
-        signRequest(request, creds) {
-          const signer = new AWS.Signers.V4(request, 'es');
-          signer.addAuthorization(creds, new Date());
-          return signer;
-        }
-      }
-      if (!acapi.config.localElasticSearch && _.get(server, 'awsCluster')) _.set(esConfig, 'Connection', AwsConnector)
-
-      // create an elasticsearch client for your Amazon ES
-      acapi.elasticSearch[instance] = new Client(esConfig)
-
-      let esData = {}
-      async.series({
-        checkVersion: (done) => {
-          acapi.elasticSearch[instance].cluster.stats((err, result) => {
-            if (err) {
-              acapi.log.error('ES | Method %s | Path %s', _.get(err, 'meta.meta.request.params.method'), _.get(err, 'meta.meta.request.params.path'))
-              acapi.log.error('ES | Error %j', _.get(err, 'meta.body'))   
-              return done(err)
-            }
-            _.merge(esData, _.get(result, 'body'))
-            return done()
-          })
-        },
-        checkIndex: (done) => {
-          acapi.elasticSearch[instance].indices.exists({ index: _.get(index, 'index') }, done)
-        }
-      }, (err) => {
-        if (err) return itDone(err)
-        acapi.aclog.serverInfo({
-          instance,
-          index: _.get(index, 'index'),
-          host,
-          port,
-          cluster: _.get(esData, 'cluster_name'),
-          clusterVersion: _.get(esData, 'version.number')
-        })
-        return itDone()
-      })
-    }, cb)
+    }
   }
 
-  const prepareForTest = (params, cb) => {
-    const instance = _.get(params, 'instance')
+  const checkForSnapshot = async({ instance }) => {
+    let response = await acapi.elasticSearch[instance].snapshot.status()
+    return _.get(response, 'body.error.root_cause[0].type') 
+  }
+  
+
+  const prepareForTest = async({ instance, createMapping }) => {
     const index = _.find(acapi.config.elasticSearch.indices, { model: instance })
 
     // reset ES in for tests
     // check for snapshot and wait (if not localDevelopment) - otherwise tests will fail
-    let esReady = false
-    async.whilst(
-      (wcb) => {
-        return wcb(null, !esReady)
-      },
-      (callback) => {
-        setTimeout(() => {
-          acapi.elasticSearch[instance].snapshot.status({
-          }, (err, result) => {
-            if (err) return callback(err)
-            if (_.get(result, 'error.root_cause[0].type') ===  'snapshot_in_progress_exception') {
-              acapi.log.warn('Bootstrap | ES | Cluster is snapshotting... we are waiting | %j', result)                    
-            }
-            else {
-              esReady = true
-            }
-            return callback()
-          })
-        }, 1000)
-      }, (err) => {
-        if (err) return cb(err)
+    try {
+      const snapshotStatus = await checkForSnapshot({ instance })
+      if (snapshotStatus ===  'snapshot_in_progress_exception') {
+        acapi.log.warn('Bootstrap | ES | Cluster is snapshotting... we are waiting | %j', snapshotStatus)  
+        await new Promise(resolve => setTimeout(resolve, 1000))
+        await prepareForTest({ instance })                  
+      }
+    }
+    catch(e) {
+      acapi.log.error('Bootstrap | ES | checkForSnapshot | Failed %j', e.message)
+      throw new Error('checkForSnapshotFaild')
+    }
+    
+    const response = await acapi.elasticSearch[instance].indices.delete({
+      index: `${index.index}*`,
+      ignore_unavailable: true
+    })
+    acapi.aclog.listing({
+      field: 'Deleted index',
+      value: _.get(response, 'body.acknowledged')
+    })
 
-        acapi.log.info('Bootstrap | ES | prepareForTest | Deleting index | %s', `${index.index}*`)            
-        acapi.elasticSearch[instance].indices.delete({
-          index: `${index.index}*`,
-          ignore_unavailable: true
-        }, (err) => {
-          if (err) return cb(err)
-          if (_.isFunction(_.get(params, 'createMapping'))) {
-            let uuidIndex = `${index.index}_${uuidv4()}`
-            acapi.log.info('Bootstrap | ES | prepareForTest | Create index | Model %s | %s', `${index.model}`, uuidIndex)            
-            _.get(params, 'createMapping')({ index: uuidIndex, model: index.model }, err => {
-              if (err) return cb(err)
-              // create alias
-              let actions = [{
-                add: { index: uuidIndex, alias: index.index }
-              }]
-              acapi.log.info('Bootstrap | ES | prepareForTest | Create alias | %j', actions)            
-              acapi.elasticSearch[instance].indices.updateAliases({
-                body: {
-                  actions
-                }
-              }, cb)
-            })
-          }
-          else {
-            return cb()
-          }
-        })    
+    // only create mapping if the function is async
+    if (_.isFunction(createMapping)) {
+      let uuidIndex = `${index.index}_${uuidv4()}`
+      acapi.aclog.listing({
+        field: 'Creating',
+        value: uuidIndex
       })
+      await createMapping({ index: uuidIndex, model: index.model })
+      // create alias
+      let actions = [{
+        add: { index: uuidIndex, alias: index.index }
+      }]
+      const response = await acapi.elasticSearch[instance].indices.updateAliases({
+        body: {
+          actions
+        }
+      })
+      acapi.aclog.listing({
+        field: 'Updated alias',
+        value: index.index
+      })
+      acapi.aclog.listing({
+        field: 'Index ready',
+        value: _.get(response, 'body.acknowledged')
+      })
+    }
   }
-
 
   return {
     init,
